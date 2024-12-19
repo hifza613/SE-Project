f29a8d768c0c7ed7e9890503bb23b4b66295e0fc
Areeba Naeem <areebanaeem069@gmail.com>
2024-12-20 02:32:18 +0500

    Add files via upload

---
+++ DynamicDictionary/DynamicDictionary/DictionaryService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class DictionaryService
+{
+    
+    private static readonly HttpClient client = new HttpClient();
+    private const string apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+    public static async Task<string> GetWordDefinitionAsync(string word)
+    {
+        try
+        {
+            var url = $"{apiUrl}{word.ToLower()}";  
+            var response = await client.GetStringAsync(url);
+
+           
+            var jsonObject = JArray.Parse(response); 
+
+            if (jsonObject.Count > 0)
+            {
+                var meanings = jsonObject[0]["meanings"];
+                if (meanings != null && meanings.HasValues)
+                {
+                    
+                    foreach (var meaning in meanings)
+                    {
+                        var wordDefinitions = meaning["definitions"];
+                        if (wordDefinitions != null && wordDefinitions.HasValues)
+                        {
+                            return wordDefinitions[0]["definition"].ToString();
+                        }
+                    }
+                }
+            }
+
+            return "No definition found.";
+        }
+        catch (JsonException jsonEx)
+        {
+            return $"Error parsing definition: {jsonEx.Message}";
+        }
+        catch (Exception ex)
+        {
+           
+            return $"Error fetching definition: {ex.Message}";
+        }
+    }
+}
